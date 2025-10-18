01105ee3d758578f4a9580eeea89a5cc7db41ecb
VuLe005 <157918086+VuLe005@users.noreply.github.com>
2025-10-17 17:35:04 -0700

    a

--- global.js
+++ global.js
@@ -4,12 +4,29 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$('nav a');
+// const navLinks = $$('nav a');
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
 
-if (currentLink) {
-  currentLink.classList.add('current');
+// if (currentLink) {
+//   currentLink.classList.add('current');
+// }
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact'},
+  { url: 'resume/', title:'Resume' },
+  { url: "https://github.com/VuLe005", title: "GitHub" },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
