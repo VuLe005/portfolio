3995b6b4c301dbb00ed0e6221ebd9b1d0ede25cd
VuLe005 <157918086+VuLe005@users.noreply.github.com>
2025-10-24 20:17:39 -0700

    a

--- global.js
+++ global.js
@@ -108,16 +108,15 @@ form?.addEventListener('submit', (event) => {
 
 export async function fetchJSON(url) {
   try {
-    // Fetch the JSON file from the given URL
     const response = await fetch(url);
-  
+    console.log(response);
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
-  
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
-
-
+    throw error;
   }
 }
